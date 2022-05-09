9142c4af7415f1feeb83f903a3636b705260bcc9
XU <63553976+Xuiao@users.noreply.github.com>
2022-05-09 16:50:11 +0200

    Update UploadFile.java

--- src/test/java/UploadFile.java
+++ src/test/java/UploadFile.java
@@ -28,7 +28,7 @@ public class UploadFile extends PageBase {
         addPic.click();
         //in this case i have already uploaded the pic once, so the system asked should change the name and the title of the file.
         driver.findElement(By.id("ctl00_cphMain_FileUploadUgyfile")).sendKeys("C:/Users/dell/.ssh/2022-wednesday-late/src/test.png");
-        driver.findElement(By.name("ctl00$cphMain$TxtUgyfileNev")).sendKeys("This is a test for selenium");
+        driver.findElement(By.name("ctl00$cphMain$TxtUgyfileNev")).sendKeys("This is a test1 for selenium");
         stop();
 
         WebElement uploadPic = waitVisibiiltyAndFindElement(By.id("ctl00_cphMain_BtnUgyfileUpload"));
